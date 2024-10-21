71fc6d7e8bd20658e9544c93ee5a112fe421b1b6
Picard <raphaelpicard@outlook.fr>
2024-10-21 16:40:41 +0200

    Clean for init v2

--- types/social.d.ts
+++
@@ -1,8 +0,0 @@
-import type { SanityImage, SantiyBody } from './sanity';
-
-export interface Social extends SantiyBody {
-  _type: 'social';
-  title: string;
-  url: string;
-  image: SanityImage;
-}
--- utils/groupBy.tsx
+++
@@ -1,9 +0,0 @@
-export function groupBy<T, K extends keyof T, data>(
-  array: T[],
-  property: K,
-  value: data
-) {
-  return array.filter((item) => {
-    if (item[property] === value) return item;
-  });
-}
